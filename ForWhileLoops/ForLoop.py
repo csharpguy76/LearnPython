bdccc7d551fc4fc7811ccc5e36b2e31b11aaf4c4
David Loo <csharpguy76@gmail.com>
2014-07-06 14:47:45 +0800

    Added For and While Loop demonstration for Python

---
+++ ForWhileLoops/ForLoop.py
@@ -0,0 +1,9 @@
+"""
+This is a for loop demonstration in Python language
+"""
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print number
+
---
+++ ForWhileLoops/WhileLoop.py
@@ -0,0 +1,11 @@
+"""
+Demonstrate how to use the While loop in Python
+"""
+
+numbers = [1, 2, 3, 4, 5]
+
+index = 0
+
+while index < 5:
+    print numbers[index]
+    index += 1
